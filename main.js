9c941ca55b88373e942cbfa16dd804a5c57201dd
Mike Spross <mgspross@gmail.com>
2016-02-28 14:12:11 -0500

    Actually update main.js to use zoom (oops)

--- main.js
+++ main.js
@@ -2,14 +2,5 @@ define(function (require, exports, module) {
     "use strict";
 
     var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
-
-    ExtensionUtils.addEmbeddedStyleSheet("#sidebar *, #main-toolbar *, #titlebar *, #problems-panel *,"+
-	"#find-in-files-results *, #e4b-main-panel *, #status-bar *,"+
-	"#main-toolbar *, #context-menu-bar *, #codehint-menu-bar *,"+
-	"#quick-view-container *, #function-hint-container *  { font-size: 22px !important;"+
-	" line-height: 25px !important; }"+
-	".sidebar li { min-height: 25px !important;}"+
-	".sidebar-selection, .filetree-selection { min-height: 30px !important; margin-top: 3px;}"+
-	""
-	);
+    ExtensionUtils.addEmbeddedStyleSheet("body { zoom: 2 !important; }");
 });
